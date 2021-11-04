5bf2cf05e8303d22d7aa7cc8ad41837e1bf25a0e
Jon Torres <jonathantorres10@gmail.com>
2021-11-04 00:12:27 -0300

    solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (...num) {
+    const arr = num[0];
+    return arr.filter((element) => !num.includes(element));
 };
 
 // Do not edit below this line
