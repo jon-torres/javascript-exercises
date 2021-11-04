810c2fe02da34afaece25a35f9c7c3feeeaabf4b
Jon Torres <jonathantorres10@gmail.com>
2021-11-04 01:02:20 -0300

    solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    //returns ERROR with negative numbers
+    if (min < 0 || max < 0) return 'ERROR';
 
+
+    // returns ERROR with non-number parameters
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+
+
+    // works with larger number first
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
