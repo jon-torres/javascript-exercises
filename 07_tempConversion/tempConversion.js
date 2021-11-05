8a52086be3813f6fab8d037e200e1d1109c09508
Jon Torres <jonathantorres10@gmail.com>
2021-11-05 00:05:54 -0300

    Solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  // parseFloat(f.toFixed(1))
+  return parseFloat(((f - 32) / 1.8).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  return parseFloat(((c * 9 / 5) + 32).toFixed(1))
 };
 
 // Do not edit below this line
