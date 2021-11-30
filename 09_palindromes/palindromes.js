33b9bcc74bc273b2b6c3dcf368e3153ba72a8801
Jon Torres <jonathantorres10@gmail.com>
2021-11-30 10:39:03 -0300

    solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+  let re = /[^A-Za-z0-9]/g;
+  palindrome = palindrome.toLowerCase().replace(re, "");
+  let len = palindrome.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (palindrome[i] !== palindrome[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+// Fancier way
+// const palindromes = function(string) {
+//     processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+//     return (
+//       processedString
+//         .split("")
+//         .reverse()
+//         .join("") == processedString
+//     );
+//   };
 
 // Do not edit below this line
 module.exports = palindromes;
